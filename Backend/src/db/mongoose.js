80f5f00a57990b3d7472d69ff404d570cef25ead
SuneQuist44 <cotz4488@gmail.com>
2020-12-28 01:17:32 +0100

    Backend plugins

--- Backend/src/db/mongoose.js
+++ Backend/src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://sune:admin@shopping.h5xhw.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log("Error occured while connection to MongoDB:", err.message));
