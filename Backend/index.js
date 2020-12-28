732c81d357ea5b32f564b553bd037b11d9a65cb5
SuneQuist44 <cotz4488@gmail.com>
2020-12-28 11:31:38 +0100

    backend v.1

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,20 @@
+const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
 require('dotenv').config();
+
+app.use(express.json()); // Conevert data into JSON
 
 mongoose.connect(process.env.HOST, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log("Error occured while connecting to MongoDB:", err.message));
+
+// Router Connections
+const User = require('./src/Router/User');
+
+// Routers
+app.use(User);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Connected to port", PORT));
