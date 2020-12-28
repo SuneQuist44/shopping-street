c613fe7aa4a889c62e4fee58c76e483569d7865e
SuneQuist44 <cotz4488@gmail.com>
2020-12-28 01:20:33 +0100

    Resetup

--- Backend/src/db/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.log("Error occured while connection to MongoDB:", err.message));
